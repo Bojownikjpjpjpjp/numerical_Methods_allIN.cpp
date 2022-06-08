b7b894140b0dabbf08b0bd7d1550b4841e0c7887
Bojownikjpjpjpjp <75701319+Bojownikjpjpjpjp@users.noreply.github.com>
2022-06-08 11:05:36 +0200

    nie dziala

--- Sklejka.cpp.cpp
+++ Sklejka.cpp.cpp
@@ -9,35 +9,318 @@
 #include "Kwadratury_Gaussa.h"
 
 using namespace std;
+double(*f[6])(double);
+double(*g[6])(double, double**, int, int);
 
-void licz_funkcje(double** baza, double* wspolczynniki_ukladu) {
 
+#pragma region Funkcje_przestrzeni_F
+double f0(double x) {
+	return pow(x, 0);
+}
+double f1(double x) {
+	return pow(x, 1);
+}
+double f2(double x) {
+	return pow(x, 2);
+}
+double f3(double x) {
+	return pow(x, 3);
+}
+double f4(double x) {
+	return pow(x, 4);
+}
+
+double f5(double x) {
+	return pow(x, 5);
+}
+
+#pragma endregion
+#pragma region Funkcje_przestrzeni_G
+double g0(double x, double** bazaG, int i_UP, int j_UP) {
+	double suma = 0;
+	//i_UP--;
+	for (int j = 0; j < i_UP; j++) {
+		suma += bazaG[i_UP][j] * pow(x, j);
+	}
+	return suma;
+	//return pow((x * bazaG[i_UP][i_UP]), i_UP);
+	//sume po j G[i][j] * pow(x, j)
+}
+double g1(double x, double** bazaG, int i_UP, int j_UP) {
+	double suma = 0;
+	i_UP--;
+	//cout << x << " " << i_UP << " " << j_UP;
+	for (int j = 0; j <= i_UP; j++) {
+		suma += bazaG[i_UP][j] * pow(x, j);
+		//cout <<j<<bazaG[i_UP][j]<<" " << suma;
+	}
+	
+	return suma;
+	//return pow((x * bazaG[i_UP][i_UP]), i_UP);
+}
+double g2(double x, double** bazaG, int i_UP, int j_UP) {
+	double suma = 0;
+	//i_UP--;
+	
+	for (int j = 0; j < i_UP; j++) {
+		suma += bazaG[i_UP][j] * pow(x, j);
+	}
+	return suma;
+	//return pow((x * bazaG[i_UP][i_UP]), i_UP);
+}
+double g3(double x, double** bazaG, int i_UP, int j_UP) {
+	double suma = 0;
+	//i_UP--;
+	for (int j = 0; j < i_UP; j++) {
+		suma += bazaG[i_UP][j] * pow(x, j);
+	}
+	return suma;
+	//return pow((x * bazaG[i_UP][i_UP]), i_UP);
+}
+double g4(double x, double** bazaG, int i_UP, int j_UP) {
+	double suma = 0;
+	//i_UP--;
+	for (int j = 0; j < i_UP; j++) {
+		suma += bazaG[i_UP][j] * pow(x, j);
+	}
+	return suma;
+	//return pow((x * bazaG[i_UP][i_UP]), i_UP);
+}
+double g5(double x, double** bazaG, int i_UP, int j_UP) {
+	double suma = 0;
+	//i_UP--;
+	for (int j = 0; j < i_UP; j++) {
+		suma += bazaG[i_UP][j] * pow(x, j);
+	}
+	return suma;
+	//return pow((x * bazaG[i_UP][i_UP]), i_UP);
+}
+
+#pragma endregion
+
+double licz_calke_z_gg(double a, double b, int n, int i_UP, int j_UP, double (*G1[])(double, double**, int, int), double (*G2[])(double, double**, int, int), double** bazaG) {
+	//a,b przedział, n ilość podpodziałów
+	double suma = 0;
+	double a1 = a;
+	double dx = ((b - a) / n);
+	if (dx < 0) {
+		dx *= -1;
+	}
+	double b1 = a1 + dx;
+
+	double suma_podstaw = 0;
+	for (int i = 0; i < n; i++) {
+		//suma_podstaw = ((F1(a1)*F2(a1)) + (F1(b1)*F2(b1)));
+		suma_podstaw = ((G1[1](a1, bazaG, i_UP, j_UP) * G1[1](a1, bazaG, i_UP, j_UP)) + (G1[1](b1, bazaG, i_UP, j_UP) * (G2[1](b1, bazaG, i_UP, j_UP))));
+
+		suma += ((suma_podstaw * dx) / 2);
+		a1 += dx;
+		b1 += dx;
+	}
+	cout << "\ncalka: " << suma;
+	return suma;
+}
+
+double licz_calke_z_gf(double a, double b, int n, int i_UP, int j_UP, double (*G[])(double, double**, int,int), double (*F2)(double), double** bazaG) {
+	//a,b przedział, n ilość podpodziałów
+	double suma = 0;
+	double a1 = a;
+	double dx = ((b - a) / n);
+	if (dx < 0) {
+		dx *= -1;
+	}
+	double b1 = a1 + dx;
+
+	double suma_podstaw = 0;
+	for (int i = 0; i < n; i++) {
+		suma_podstaw = ((G[1](a1, bazaG, i_UP, j_UP) * F2(a1)) + (G[1](a1, bazaG, i_UP, j_UP) * F2(b1)));
+		suma += ((suma_podstaw * dx) / 2);
+		a1 += dx;
+		b1 += dx;
+	}
+	cout << "\ncalka: " << suma;
+	return suma;
+}
+
+double licz_wspolczynniki(double** bazaF, double** bazaG, double a, double b, double (*f[])(double), double (*g[])(double, double**, int, int), int n) {
+	double licznik = 0.0;
+	double mianownik = 0.0;
+	double w_sumy = 0.0;
+	double suma = 0.0;
+
+	for (int i = 1; i < 6; i++) {//pętla liczenia współczynników
+		
+		for (int j = 0; j < i; j++) {//pętla sumy
+			licznik = licz_calke_z_gf(a, b, n, i, j, g, f[i], bazaG);
+			cout << "\n Licznik: " << licznik;
+			mianownik = licz_calke_z_gg(a, b, n, i, j, g, g, bazaG);
+			cout << "\n mianownik: " << mianownik;
+			w_sumy = ((licznik / mianownik)* bazaG[j][j]);
+			suma += w_sumy;
+			cout << "\n wyraz sumy: " << w_sumy;
+			cout << "\n suma: " << suma;
+			//cout << "\n wyraz bazy G: " << bazaG[i][i];
+			//cout << "\n wyraz bazy F: " << bazaF[i][i];
+			bazaG[i][j] = w_sumy;
+			w_sumy = 0.0;
+			//bazaG[i][j] = w_sumy;
+		}
+
+		bazaG[i][i] = (bazaF[i][i] - suma);
+		cout << "\n po przypisaniu wyraz bazy G: " << bazaG[i][i];
+		cout << "\n ";
+		suma = 0.0;
+	}
+	return suma;
 }
 int main()
 {
 #pragma region Ortogonalizacje
+#pragma region tworzenie_i_zerowanie_baz
 	//baza funkcji. całość w postaci wielomianu :a0*1, a1*x, a2*x^2...a4*x^4
 	//tworzenie bazy
 	double** bazaF;
-	bazaF = new double* [5];
-	for (int i = 0; i < 5; i++) {
-		bazaF[i] = new double [5];
+	double** bazaG;
+	double** bazaG2;
+	bazaF = new double* [6];
+	for (int i = 0; i < 6; i++) {
+		bazaF[i] = new double[6];
 	}
 	//zerowanie bazy
-	for (int i = 0; i < 5; i++) {
-		for (int j = 0; j < 5; j++) {
+	for (int i = 0; i < 6; i++) {
+		for (int j = 0; j < 6; j++) {
 			bazaF[i][j] = 0.0;
 		}
 	}
-	//poszczególne współczynniki na diagonalnej =1, dla reszty =0
+	bazaG = new double* [6];
+	for (int i = 0; i < 6; i++) {
+		bazaG[i] = new double[6];
+	}
+	//zerowanie bazy
+	for (int i = 0; i < 6; i++) {
+		for (int j = 0; j < 6; j++) {
+			bazaG[i][j] = 0.0;
+		}
+	}
+	bazaG2 = new double* [6];
+	for (int i = 0; i < 6; i++) {
+		bazaG2[i] = new double[6];
+	}
+	//zerowanie bazy
+	for (int i = 0; i < 6; i++) {
+		for (int j = 0; j < 6; j++) {
+			bazaG2[i][j] = 0.0;
+		}
+	}
+#pragma endregion
+#pragma region przypisanie_wskaźników_funkcji
+	//funkcje przestrzeni: 1,x,x^2,x^3,x^4
+	f[0] = f0;
+	f[1] = f1;
+	f[2] = f2;
+	f[3] = f3;
+	f[4] = f4;
+	f[5] = f5;
+	//funkcje przestrzeni: 1,x,x^2,x^3,x^4
+	g[0] = g0;
+	g[1] = g1;
+	g[2] = g2;
+	g[3] = g3;
+	g[4] = g4;
+	g[5] = g5;
+#pragma endregion
+#pragma region Ustawienie_Diagonalnej_bazy_standardowej_i_pierwszego_diagonalnego_wyrazu_macierzy_g
+//baza F to baza podstawowa 1,x,x^2,x^3,x^4
+//baza g ma w sobie współczynniki ortogonalizacji bazy f, i po algorytmie jest bazą ortogonalną przestrzeni funkcyjnej
+
+//[wektory macierzy][współczynniki]
+//współczynniki bazy standardowej (na diagonalnej 1, reszta 0
 	bazaF[0][0] = 1;
 	bazaF[1][1] = 1;
 	bazaF[2][2] = 1;
 	bazaF[3][3] = 1;
 	bazaF[4][4] = 1;
+	bazaF[5][5] = 1;
+	//[wektory macierzy][współczynniki]
+	//współczynniki
+	bazaG[0][0] = bazaF[0][0];
+
+#pragma endregion
+	double a = -1.0;
+	double b = 1.0;
 	
-	double* wspolczynniki_ukladu = new double[5];
+	cout<<licz_wspolczynniki(bazaF, bazaG, a, b, f, g , 20000);
 	
+
+#pragma region wypisanie_baz
+	cout << "baza Podstawowa: \n";
+	for (int i = 0; i < 6; i++) {
+		for (int j = 0; j < 6; j++) {
+			//if (bazaF[i][j] != 0) {
+			//	cout << bazaF[i][j] << "\n";
+				cout << bazaF[i][j] << " ";
+			//}
+			//else {
+			//	continue;
+			//}
+		}
+		cout << " \n";
+	}
+	//cout << "\nG [1][1] "<<bazaG[1][1]<<"\n";
+	cout << "\nbaza ortogonalna: \n";
+	for (int i = 0; i < 6; i++) {
+		for (int j = 0; j < 6; j++) {
+			//if (bazaG[i][j] != 0) {
+				//cout << bazaG[i][j] << "\n";
+				cout << bazaG[i][j] << " ";
+			//}
+			//else {
+			//	continue;
+			//}
+		}
+		cout <<" \n";
+	}
+#pragma endregion
+
+#pragma region Reortogonalizacja_próba
+/*
+	bazaG2[0][0] = bazaG[0][0];
+	bazaG2[1][1] = bazaG[1][1];
+	cout << licz_wspolczynniki(bazaG, bazaG2, a, b, f, 9000);
+
+
+#pragma region wypisanie_baz_2
+	cout << "baza Podstawowa: \n";
+	for (int i = 0; i < 6; i++) {
+		for (int j = 0; j < 6; j++) {
+			//if (bazaF[i][j] != 0) {
+			//	cout << bazaF[i][j] << "\n";
+			cout << bazaG[i][j] << " ";
+			//}
+			//else {
+			//	continue;
+			//}
+		}
+		cout << " \n";
+	}
+	//cout << "\nG [1][1] "<<bazaG[1][1]<<"\n";
+	cout << "\nbaza ortogonalna: \n";
+	for (int i = 0; i < 6; i++) {
+		for (int j = 0; j < 6; j++) {
+			//if (bazaG[i][j] != 0) {
+				//cout << bazaG[i][j] << "\n";
+			cout << bazaG2[i][j] << " ";
+			//}
+			//else {
+			//	continue;
+			//}
+		}
+		cout << " \n";
+	}
+#pragma endregion
+*/
+#pragma endregion
+
 #pragma endregion
 
 
